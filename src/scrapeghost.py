d94640c68dd934b4b20cd744c1f9b4fad5d0e611
James Turk <dev@jamesturk.net>
2023-03-18 00:53:52 -0500

    autosplit

--- src/scrapeghost.py
+++ src/scrapeghost.py
@@ -8,6 +8,34 @@ class InvalidJSONError(Exception):
     pass
 
 
+def _recombine(tags, auto_split) -> list[str]:
+    """
+    Given a list of all matching HTML tags, recombine into HTML chunks
+    that can be passed to API.
+
+    Returns list of strings, will always be len()==1 if auto_split is False
+    """
+    if not auto_split:
+        return ["\n".join(lxml.html.tostring(n, encoding="unicode") for n in tags)]
+
+    # dumb hack to send input in chunks
+    pieces = []
+    cur_piece = ""
+    cur_piece_len = 0
+    for tag in tags:
+        tag_html = lxml.html.tostring(tag, encoding="unicode")
+        tag_len = len(tag_html)
+        if cur_piece_len + tag_len > auto_split:
+            pieces.append(cur_piece)
+            cur_piece = ""
+            cur_piece_len = 0
+        cur_piece += tag_html
+        cur_piece_len += tag_len
+
+    pieces.append(cur_piece)
+    return pieces
+
+
 class AutoScraper:
     def scrape(
         self,
@@ -15,8 +43,8 @@ class AutoScraper:
         *,
         xpath: str | None = None,
         css: str | None = None,
+        auto_split: int = 0,
         model: str = "gpt-4",
-        input_token_limit: int = 8192,
         max_tokens: int = 2048,
         temperature: float = 0,
     ) -> dict:
@@ -31,6 +59,27 @@ class AutoScraper:
         Returns:
             dict: The scraped data in the specified schema.
         """
+
+        def _response_to_json(response: str) -> list | dict:
+            print(f"making request now: len={len(response)}")
+            completion = openai.ChatCompletion.create(
+                model=model,
+                messages=[
+                    {"role": "system", "content": msg} for msg in self.system_messages
+                ]
+                + [
+                    {"role": "user", "content": html},
+                ],
+                max_tokens=max_tokens,
+                temperature=temperature,
+            )
+            response = completion.choices[0]["message"]["content"]
+
+            try:
+                return json.loads(response)
+            except json.decoder.JSONDecodeError:
+                raise InvalidJSONError(response)
+
         if xpath and css:
             raise ValueError("Cannot specify both xpath and css")
 
@@ -40,27 +89,22 @@ class AutoScraper:
         doc = lxml.html.fromstring(html)
 
         if xpath:
-            html = "\n".join(
-                lxml.html.tostring(n, encoding="unicode") for n in doc.xpath(xpath)
-            )
+            chunks = _recombine(doc.xpath(xpath), auto_split)
         elif css:
-            html = "\n".join(
-                lxml.html.tostring(n, encoding="unicode") for n in doc.cssselect(css)
-            )
+            chunks = _recombine(doc.cssselect(css), auto_split)
         else:
-            html = lxml.html.tostring(doc, encoding="unicode")
+            # forced to use single chunk if no selector is specified
+            chunks = [lxml.html.tostring(doc, encoding="unicode")]
 
-        if len(html) / 3 > input_token_limit:
-            raise ValueError("HTML is too long for the 8k limit")
+        print(
+            f"broken into {len(chunks)} chunks: "
+            + ", ".join(str(len(c)) for c in chunks)
+        )
 
-        print(len(html), "after simplification")
-
-        response = self._handle_html(html, model, max_tokens, temperature)
-
-        try:
-            return json.loads(response)
-        except json.decoder.JSONDecodeError:
-            raise InvalidJSONError(response)
+        if len(chunks) == 1:
+            return _response_to_json(chunks[0])
+        # flatten list of lists
+        return [item for chunk in chunks for item in _response_to_json(chunk)]
 
     # allow the class to be called like a function
     __call__ = scrape
@@ -71,18 +115,6 @@ class AutoScraper:
         """
         Pass HTML to the OpenAI API and return the response.
         """
-        completion = openai.ChatCompletion.create(
-            model=model,
-            messages=[
-                {"role": "system", "content": msg} for msg in self.system_messages
-            ]
-            + [
-                {"role": "user", "content": html},
-            ],
-            max_tokens=max_tokens,
-            temperature=temperature,
-        )
-        return completion.choices[0]["message"]["content"]
 
 
 class SchemaScraper(AutoScraper):
