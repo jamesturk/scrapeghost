055ee83b4687370429a196ec175d639c93b9de98
James Turk <dev@jamesturk.net>
2023-03-18 00:25:27 -0500

    move parameters to scrape()

--- src/scrapeghost.py
+++ src/scrapeghost.py
@@ -9,13 +9,16 @@ class InvalidJSONError(Exception):
 
 
 class AutoScraper:
-    def __init__(self):
-        self.max_tokens = 2048
-        self.temperature = 0
-        self.system_messages = []
-
     def scrape(
-        self, url: str, xpath: str | None = None, css: str | None = None
+        self,
+        url: str,
+        *,
+        xpath: str | None = None,
+        css: str | None = None,
+        model: str = "gpt-4",
+        input_token_limit: int = 8192,
+        max_tokens: int = 2048,
+        temperature: float = 0,
     ) -> dict:
         """
         Scrape a URL and return a JSON object.
@@ -47,13 +50,13 @@ class AutoScraper:
         else:
             html = lxml.html.tostring(doc, encoding="unicode")
 
-        print(len(html), "after simplification")
-
-        if len(html) > 8192:
-            print(html)
+        if len(html) / 3 > input_token_limit:
             raise ValueError("HTML is too long for the 8k limit")
 
-        response = self.handle_html(html)
+        print(len(html), "after simplification")
+
+        response = self._handle_html(html, model, max_tokens, temperature)
+
         try:
             return json.loads(response)
         except json.decoder.JSONDecodeError:
@@ -62,40 +65,41 @@ class AutoScraper:
     # allow the class to be called like a function
     __call__ = scrape
 
-    def handle_html(self, html: str) -> str:
+    def _handle_html(
+        self, html: str, model: str, max_tokens: int, temperature: float
+    ) -> str:
         """
         Pass HTML to the OpenAI API and return the response.
         """
         completion = openai.ChatCompletion.create(
-            model="gpt-4",
+            model=model,
             messages=[
                 {"role": "system", "content": msg} for msg in self.system_messages
             ]
             + [
                 {"role": "user", "content": html},
             ],
-            max_tokens=self.max_tokens,
-            temperature=self.temperature,
+            max_tokens=max_tokens,
+            temperature=temperature,
         )
         return completion.choices[0]["message"]["content"]
 
 
 class SchemaScraper(AutoScraper):
-    def __init__(self, schema):
-        super().__init__()
-        self.schema = schema
+    def __init__(self, schema, extra_instructions=None):
         self.system_messages = [
             "When you receive HTML, generate an equivalent JSON object matching this schema: {schema}".format(
                 schema=json.dumps(schema)
             ),
         ]
+        if extra_instructions:
+            self.system_messages.append(extra_instructions)
 
 
 class LinkExtractor(AutoScraper):
-    def __init__(self, specific_instructions: str | None = None):
-        super().__init__()
+    def __init__(self, extra_instructions=None):
         self.system_messages = [
             'When you receive HTML, extract a list of links matching this schema: [{{"url": "url", "text": "string"}}]',
         ]
-        if specific_instructions:
-            self.system_messages.append(specific_instructions)
+        if extra_instructions:
+            self.system_messages.append(extra_instructions)
