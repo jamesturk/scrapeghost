9986f4cc63ee12dd7d887d97e4139c0bee214a74
James Turk <dev@jamesturk.net>
2023-11-24 16:49:56 -0600

    2023-1106 updates

--- src/scrapeghost/models.py
+++ src/scrapeghost/models.py
@@ -18,9 +18,8 @@ class Model:
 models = [
     Model("gpt-4", 0.03, 0.06, 8192),
     Model("gpt-4-32k", 0.06, 0.12, 32768),
-    Model("gpt-3.5-turbo", 0.0015, 0.002, 4096),
-    Model("gpt-3.5-turbo-16k", 0.003, 0.004, 16384),
-    Model("gpt-3.5-turbo-0613", 0.0015, 0.002, 4096),
-    Model("gpt-3.5-turbo-16k-0613", 0.003, 0.004, 16384),
+    Model("gpt-4-1106-preview", 0.01, 0.03, 128000),
+    Model("gpt-3.5-turbo", 0.001, 0.002, 16384),
+    Model("gpt-3.5-turbo-1106", 0.001, 0.002, 16384),
 ]
 _model_dict = {model.name: model for model in models}
